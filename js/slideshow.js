dc1b86504bb382c5e7435493725e3eaa4b08ec5c
Dmitriy Pozdin <podmev@gmail.com>
2023-02-21 15:52:06 +0300

    fixed carousel in slideshow.js

--- js/slideshow.js
+++ js/slideshow.js
@@ -6,11 +6,14 @@ function carousel() {
   var i;
   var x = document.getElementsByClassName("mySlides");
   console.log(x.length)
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+  //if x.length==0 can be exception
+  if(x.length > 0){
+      for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+      }
+      slideIndex++;
+      if (slideIndex > x.length) {slideIndex = 1}
+      x[slideIndex-1].style.display = "block";
   }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
-  x[slideIndex-1].style.display = "block";
   setTimeout(carousel, 2000); // Change image every 2 seconds
 }
